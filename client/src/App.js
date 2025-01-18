0d8790adf6f6248a2035efccdae2ae753744ac9d
Emily Murray <emilymurray@uvic.ca>
2025-01-18 12:43:20 -0800

    make a button

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import '.App.css';
+import './App.css';
 import axios from 'axios';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   // do some stuff
 
+  const [display, setDisplay] = useState('');
+
+  const handleButtonClick = (val) => {
+    setDisplay(display + val);
+
+  }
+
+  const onSubmit = async () => {
+    
+  }
+   
   return (
 
     <div>
@@ -13,7 +24,8 @@ function App() {
         {display}
       </div>
       <div>
-        <button onClick={() => {}}>5</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={onSubmit}>submit</button>
       </div>
     </div>
 
